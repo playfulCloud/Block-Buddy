5400b14d9f486e119d4167822c0ec0ca637a1525
NimboStratus <generovic@gmail.com>
2024-02-07 21:21:21 +0100

    initial

---
+++ src/main/java/com/blockbuddy/alpha/AlphaApplication.java
@@ -0,0 +1,13 @@
+package com.blockbuddy.alpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blockbuddy/alpha/AlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blockbuddy.alpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
